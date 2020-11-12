f1587ad8c9698e604a396b5d0a2f6f39c48187af
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2020-11-12 19:23:15 +0530

    Subject card component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+import Subjectcard from './Components/subjectCard/subjectCard';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="assign App">
+      <Subjectcard name="Computer Science and Engineering" img="https://images.theconversation.com/files/49135/original/22qc7r28-1400667334.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=600&h=429&fit=crop&dpr=1" link1="#" link2="#" link3="#"/>
+      <Subjectcard name="Mechanical Engineering" img="https://images.theconversation.com/files/49135/original/22qc7r28-1400667334.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=600&h=429&fit=crop&dpr=1" link1="#" link2="#" link3="#"/>
+      </div>
   );
 }
 
